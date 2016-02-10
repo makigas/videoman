b15d3f292cb1df144d0b0a3dc5896877a83565b7
Dani Rodríguez <danirod@outlook.com>
2016-02-10 20:43:17 +0100

    (Needs testing) Add settings page
    The widget has settings for the following things:
    - Title: you can set the title to be displayed above the recent videos.
    - Button link: you can set the link the button will point to.
    - Button text: you can set the text the button will display.

--- src/lib/Makigas/VideoManager/RecentVideosWidget.php
+++ src/lib/Makigas/VideoManager/RecentVideosWidget.php
@@ -4,8 +4,14 @@ namespace Makigas\VideoManager;
 
 defined('ABSPATH') or die('No way, punk');
 
+/**
+ * This widget displays recent videos from the database.
+ */
 class RecentVideosWidget extends \WP_Widget {
     
+    /**
+     * Create a new instance for the widget.
+     */
     public function __construct() {
         $widget_ops = array(
             'classname' => 'makigas-recent-videos',
@@ -21,9 +27,12 @@ class RecentVideosWidget extends \WP_Widget {
      * @param type $instance
      */
     public function widget( $args, $instance ) {
+        $title = apply_filters( 'widget_title', empty($instance['title']) ? '' : $instance['title'], $this->id_base);
+        $button_text = empty($instance['button_text']) ? '' : $instance['button_text'];
+        $button_link = empty($instance['button_href']) ? '#' : $instance['button_href'];
         ?>
         <div class="home-section">
-                <h1><?php _e( 'Recent videos', 'makigas-videoman' ) ?></h1>
+                <h1><?php echo $title; ?></h1>
                 <div class="row">
                     <?php $recent_videos = new \WP_Query('post_type=video&showposts=3'); ?>
                     <?php while ($recent_videos->have_posts()): ?>
@@ -38,7 +47,7 @@ class RecentVideosWidget extends \WP_Widget {
                     <?php endwhile; ?>
                 </div>
                 <p class="text-center">
-                    <a href="/series/" class="btn btn-lg btn-primary"><?php _e( 'Ver todas las series &raquo;', 'makigas-videoman' ); ?></a>
+                    <a href="<?php echo $button_link; ?>" class="btn btn-lg btn-primary"><?php echo $button_text; ?></a>
                 </p>
                 </section>
             </div>
@@ -49,10 +58,26 @@ class RecentVideosWidget extends \WP_Widget {
      * To be executed when the admin form has to be rendered.
      * @param type $instance
      */
-    public function form( $instance ) {
-        /* No settings. */
+    public function form( $inst ) {
+        /* Prepare settings to be rendered. */
+        $instance = wp_parse_args((array) $inst, array(
+            'title' => __( 'Recent Videos', 'makigas-videoman' ),
+            'button_text' => __( 'See recent episodes', 'makigas-videoman' ),
+            'button_href' => __( '#', 'makigas-videoman' )
+        ));
+        
+        $title = sanitize_text_field( $instance[ 'title' ] );
+        $button_text = sanitize_text_field( $instance[ 'button_text' ] );
+        $button_link = sanitize_text_field( $instance[ 'button_href' ] );
         ?>
-        <p><?php _e( 'This plugin has no configurable settings at the moment.', 'makigas-videoman' ); ?></p>
+        <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e( 'Title:' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+        
+        <p><label for="<?php echo $this->get_field_id('button_text'); ?>"><?php _e( 'Button Text:', 'makigas-videoman' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('button_text'); ?>" name="<?php echo $this->get_field_name('button_text'); ?>" type="text" value="<?php echo esc_attr($button_text); ?>" /></p>
+        
+        <p><label for="<?php echo $this->get_field_id('button_href'); ?>"><?php _e( 'Button Link:', 'makigas-videoman' ); ?></label>
+            <input class="widefat" id="<?php echo $this->get_field_id('button_href'); ?>" name="<?php echo $this->get_field_name('button_href'); ?>" type="text" value="<?php echo esc_attr($button_link); ?>" /></p>
         <?php
     }
     
@@ -62,7 +87,13 @@ class RecentVideosWidget extends \WP_Widget {
      * @param type $old_inst
      */
     public function update( $new_inst, $old_inst ) {
-        /* No options means nothing to save. */
+        $out_instance = $old_inst;
+        
+        $out_instance[ 'title' ] = sanitize_text_field( $new_inst[ 'title' ] );
+        $out_instance[ 'button_text' ] = sanitize_text_field( $new_inst[ 'button_text' ] );
+        $out_instance[ 'button_href' ] = sanitize_text_field( $new_inst[ 'button_href' ] );
+        
+        return $out_instance;
     }
     
 }
